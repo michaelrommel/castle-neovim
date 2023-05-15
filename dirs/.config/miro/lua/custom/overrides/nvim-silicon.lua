0e581e7052f73806be060f2dd1f38779da4bb839
Michael Rommel <rommel@layer-7.net>
2023-05-16 00:45:56 +0200

    fix: override for silicon only on WSL

--- dirs/.config/miro/lua/custom/overrides/nvim-silicon.lua
+++ dirs/.config/miro/lua/custom/overrides/nvim-silicon.lua
@@ -1,6 +1,14 @@
-return {
-	-- create code images
-	font = "VictorMono NF;Noto Emoji",
-	theme = "gruvbox-dark",
-	to_clipboard = false,
-}
+-- overrides for silicon - tool to create code images
+local is_wsl = (function()
+	local output = vim.fn.systemlist "uname -r"
+	return not not string.find(output[1] or "", "WSL")
+end)()
+
+if is_wsl then
+	return {
+		font = "VictorMono NF;Noto Emoji",
+		to_clipboard = false,
+	}
+else
+	return {}
+end
--- dirs/.config/miro/lua/plugins/nvim-silicon.lua
+++ dirs/.config/miro/lua/plugins/nvim-silicon.lua
@@ -7,7 +7,7 @@ local opts = {
 -- if an override exists, merge it in here
 local exists, override = pcall(require, "custom.overrides.nvim-silicon")
 if exists then
-	opts.opts = {}
+	opts.opts = opts.opts or {}
 	for k, v in pairs(override) do opts.opts[k] = v end
 end
 return opts
