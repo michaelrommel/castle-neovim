529f99624df5b00f0b3987cfdd12c600e0aeb328
Michael Rommel <rommel@layer-7.net>
2023-05-20 13:30:58 +0200

    fix: add macOS install

--- bootstrap/post-clone.sh
+++ bootstrap/post-clone.sh
@@ -1,20 +1,38 @@
 #! /bin/bash
 
-echo "Installing apt packages"
-sudo apt-get -y update
-sudo apt-get -y install git curl ninja-build gettext cmake unzip \
-	build-essential autoconf automake universal-ctags \
-	fontconfig python3-pip || exit
+source "${HOME}/.homesick/helper.sh"
 
-echo "Compiling and installing neovim"
-cd "${HOME}" || exit
-mkdir -p "${HOME}/software/"
-cd "${HOME}/software/" || exit
-git clone https://github.com/neovim/neovim neovim_src
-cd neovim_src || exit
-git checkout stable
-make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${HOME}/software/neovim"
-make install
+echo "Installing dependency packages"
+if is_mac; then
+	desired=(curl git shellcheck@0.9 shfmt@1.33 fnm@1.33i universal-ctags \
+		fontconfig@2.14 python@3.11 tree-sitter@0.20 neovim@0.9)
+	missing=()
+	check_brewed "missing" "${desired[@]}"
+	if [[ "${#missing[@]}" -gt 0 ]]; then
+		echo "(brew) installing ${missing[*]}"
+		brew install "${missing[@]}"
+	fi
+else
+	desired=(curl git universal-ctags ninja-build gettext cmake unzip \
+	build-essential autoconf automake fontconfig python3-pip)
+	missing=()
+	check_dpkged "missing" "${desired[@]}"
+	if [[ "${#missing[@]}" -gt 0 ]]; then
+		echo "(apt) installing ${missing[*]}"
+		sudo apt-get -y update
+		sudo apt-get -y install "${missing[@]}"
+	fi
+
+	echo "Compiling and installing neovim"
+	cd "${HOME}" || exit
+	mkdir -p "${HOME}/software/"
+	cd "${HOME}/software/" || exit
+	git clone https://github.com/neovim/neovim neovim_src
+	cd neovim_src || exit
+	git checkout stable
+	make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${HOME}/software/neovim"
+	make install
+fi
 
 if ! fnm -V >/dev/null 2>&1; then
 	echo "Installing the fast Node Manager (fnm)"
@@ -29,12 +47,17 @@ if ! rustup -V >/dev/null 2>&1; then
 	rustup default stable
 fi
 
-echo "Installing tree-sitter cli"
-cargo install tree-sitter-cli
+if ! tree-sitter -V >/dev/null 2>&1; then
+	echo "Installing tree-sitter cli"
+	cargo install tree-sitter-cli
+fi
 
-echo "Installing python neovim module"
-python3 -mpip install neovim
+if ! python3 -c 'import pynvim;' >/dev/null 2>&1; then
+	echo "Installing python neovim module"
+	python3 -mpip install pynvim
+fi
 
-echo "Installing neovim-remote"
-python3 -mpip install neovim-remote
-
+if ! nvr --version >/dev/null 2>&1; then
+	echo "Installing neovim-remote"
+	python3 -mpip install neovim-remote
+fi
--- bootstrap/post-link.sh
+++ bootstrap/post-link.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
+if ! node --version >/dev/null 2>&1; then
+	echo "Installing node"
+	source "${HOME}/.path.d/20_fnm.sh"
+	fnm install 'lts/*'
+	fnm default lts-latest
+fi
+
 echo "Updating font cache"
 fc-cache -f
 
-echo "Installing node"
-source "${HOME}/.path.d/20_fnm.sh"
-fnm install 'lts/*'
-fnm default lts-latest
