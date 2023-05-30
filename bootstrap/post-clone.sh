f507df1da7d221b1624f5c10c61e6cdef04ad1fb
Michael Rommel <rommel@layer-7.net>
2023-05-30 23:25:24 +0200

    fix: make neovim clone smaller

--- bootstrap/post-clone.sh
+++ bootstrap/post-clone.sh
@@ -31,7 +31,7 @@ else
 	cd "${HOME}" || exit
 	mkdir -p "${HOME}/software/"
 	cd "${HOME}/software/" || exit
-	git clone https://github.com/neovim/neovim neovim_src
+	git clone --filter=tree:0 https://github.com/neovim/neovim neovim_src
 	cd neovim_src || exit
 	git checkout stable
 	make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${HOME}/software/neovim"
