9419df2f524c2cdf473d5a1f8abfad8715870c59
Michael Rommel <rommel@layer-7.net>
2023-07-16 15:17:14 +0200

    fix: harmonize font usage

--- dirs/.config/miro/lua/custom/overrides/nvim-silicon.lua
+++ dirs/.config/miro/lua/custom/overrides/nvim-silicon.lua
@@ -4,7 +4,17 @@ local is_wsl = (function()
 	return not not string.find(output[1] or "", "WSL")
 end)()
 
-if is_wsl then
+local is_mac = (function()
+	local output = vim.fn.systemlist "uname -s"
+	return not not string.find(output[1] or "", "Darwin")
+end)()
+
+local is_linux = (function()
+	local output = vim.fn.systemlist "uname -s"
+	return not not string.find(output[1] or "", "Linux")
+end)()
+
+if is_wsl or is_mac or is_linux then
 	return {
 		font = "VictorMono NF;Noto Emoji",
 		to_clipboard = false,
