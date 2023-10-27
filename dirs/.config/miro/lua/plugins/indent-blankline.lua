0524ce516cd3c2eb138bd959d5e436a9ae4a6651
Michael Rommel <rommel@layer-7.net>
2023-10-27 15:08:57 +0200

    fix: cater for updated indent-blankline

--- dirs/.config/miro/lua/plugins/indent-blankline.lua
+++ dirs/.config/miro/lua/plugins/indent-blankline.lua
@@ -1,10 +1,18 @@
 -- visually draws vertical lines for code blocks
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
 	lazy = true,
 	event = "BufEnter",
 	opts = {
-		show_current_context = true,
-		show_current_context_start = false,
+		indent = {
+			tab_char = '▎',
+		},
+		scope = {
+			enabled = true,
+			show_start = false,
+			show_end = false,
+			highlight = "GruvboxRed",
+		}
 	}
 }
