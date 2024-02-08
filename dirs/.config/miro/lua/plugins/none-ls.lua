2bc4f7b753b4568f443955f348fbab87f1e5ca46
Michael Rommel <rommel@layer-7.net>
2024-02-08 19:21:02 +0100

    fix: change order to install null-ls formatters

--- dirs/.config/miro/lua/plugins/none-ls.lua
+++ dirs/.config/miro/lua/plugins/none-ls.lua
@@ -5,6 +5,7 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"williamboman/mason.nvim",
+		"jay-babu/mason-null-ls.nvim",
 		-- asynchronous lib for lua
 		"nvim-lua/plenary.nvim",
 	},
