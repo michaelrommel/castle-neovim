5aa6ab0dbb8bf0f9a7698c81b6251a7ff44ad0bb
Michael Rommel <rommel@layer-7.net>
2025-04-01 21:07:25 +0200

    fix: now have 😅 real emojis everywhere but markdown

--- dirs/.config/miro/lua/plugins/blink-cmp.lua
+++ dirs/.config/miro/lua/plugins/blink-cmp.lua
@@ -1,3 +1,45 @@
+-- generic completion engine written in Rust with fuzzy matching
+
+local dump = require('core.utils').dump
+
+-- Use this function to check if the cursor is inside a comment block
+local function inside_comment_block()
+	if vim.api.nvim_get_mode().mode ~= 'i' then
+		return false
+	end
+	local node_under_cursor = vim.treesitter.get_node()
+	local parser = vim.treesitter.get_parser(nil, nil, { error = false })
+	local query = vim.treesitter.query.get(vim.bo.filetype, 'highlights')
+	if not parser or not node_under_cursor or not query then
+		return false
+	end
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	row = row - 1
+	for id, node, _ in query:iter_captures(node_under_cursor, 0, row, row + 1) do
+		if query.captures[id]:find('comment') then
+			local start_row, start_col, end_row, end_col = node:range()
+			if start_row <= row and row <= end_row then
+				if start_row == row and end_row == row then
+					if start_col <= col and col <= end_col then
+						return true
+					end
+				elseif start_row == row then
+					if start_col <= col then
+						return true
+					end
+				elseif end_row == row then
+					if col <= end_col then
+						return true
+					end
+				else
+					return true
+				end
+			end
+		end
+	end
+	return false
+end
+
 return {
 	'saghen/blink.cmp',
 	-- optional: provides snippets for the snippet source
@@ -65,38 +107,56 @@ return {
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { 'lsp', 'path', 'snippets', 'buffer', 'emoji', 'dictionary' },
+			default = function()
+				-- put those which will be shown always
+				local result = { 'lsp', 'path', 'snippets', 'buffer' }
+				-- blank empty buffer, good for just checking spellings or words
+				if (not vim.o.filetype) or (vim.o.filetype == '') or
+					-- turn on dictionary in markdown or text file
+					vim.tbl_contains({ 'gitcommit', 'markdown', 'text' }, vim.bo.filetype) or
+					-- or turn on dictionary if cursor is in the comment block
+					inside_comment_block()
+				then
+					table.insert(result, 'emoji')
+					table.insert(result, 'dictionary')
+				end
+				return result
+			end,
 			providers = {
 				emoji = {
 					module = "blink-emoji",
 					name = "Emoji",
 					score_offset = 5,
 					opts = {
-						insert = false,
+						-- the default is to leave the :flushed: syntax
+						insert = false
 					},
-					should_show_items = function()
-						return vim.tbl_contains(
-							{ "gitcommit", "markdown" },
-							vim.o.filetype
+					transform_items = function(_, items)
+						local newtable = vim.tbl_map(
+							function(item)
+								-- for all except markdown replace with real unicode character
+								-- this essentially reverts the insert=false from above
+								if (not vim.tbl_contains({ "markdown" }, vim.bo.filetype)) then
+									-- print(string.format("%s -> %s", item.textEdit.newText, item.insertText))
+									item.textEdit.newText = item.insertText
+								end
+								return item
+							end,
+							items
 						)
-					end
+						return items
+					end,
 				},
 				dictionary = {
 					module = 'blink-cmp-dictionary',
 					name = 'Dict',
-					-- Make sure this is at least 2.
-					-- 3 is recommended
+					-- Make sure this is at least 2 - 3 is recommended
 					min_keyword_length = 3,
 					max_items = 8,
 					score_offset = -5,
 					opts = {
 						dictionary_directories = { vim.fn.expand('~/.config/dictionaries') }
 					},
-					should_show_items = function()
-						if ((not vim.o.filetype) or (vim.o.filetype == '') or (vim.tbl_contains({ "gitcommit", "markdown" }, vim.o.filetype))) then
-							return true
-						end
-					end
 				},
 				cmdline = {
 					-- ignores cmdline completions when executing shell commands
